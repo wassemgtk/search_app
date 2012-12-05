f73f88ac374238fba43be8113f35040586aef12a
Peter Godden <pgodden@zendesk.com>
2012-12-05 00:05:28 -0800

    Getting the description to work
    Trying to make the conditional value of the description alter the text
    entered for the search

--- app.js
+++ app.js
@@ -35,15 +35,31 @@
      console.log(priority);
      this.ajax ('searchDesk',priority);
 
-     var data = status + ' ' + priority;
+     
+
+      var getDescription = this.$('#description').val();
+      var descOperator = this.$('#text_operator').val();
+      
+
+      if (descOperator === "plus") {
+        var endDescription1 = "+" + getDescription.replace ( / /g, " +");
+      console.log(endDescription1);
+      this.ajax ('searchDesk',endDescription1);
+      } else if (descOperator === "minus") {
+        var endDescription2 = "-" + getDescription.replace ( / /g, " -");
+      console.log(endDescription2);
+      this.ajax ('searchDesk',endDescription2);
+      } else {
+        var endDescription3 = getDescription;
+      console.log(endDescription3);
+      this.ajax ('searchDesk',endDescription3);
+      }
+  
+
+
+     var data = 'type:ticket' + ' ' + status + ' ' + priority; 
      this.ajax('searchDesk',data);
    }
-
-
-
-
-      
-     
 
   };
 
