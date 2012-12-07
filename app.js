ab1bbca0e24c669c025ffd60894d252cb0fe64c5
Peter Godden <pgodden@zendesk.com>
2012-12-06 19:19:28 -0800

    Search is working, Results are showing
    Still need to fix it so that if values for date and description are not
    set, then it won't stuff up the search.
    
    Also need to make it more sexy
    
    Also need to automatically map values from an existing custom field

--- app.js
+++ app.js
@@ -10,17 +10,25 @@
       'click .searchbutton': 'doTheSearch',
       'mouseenter .tips': 'showToolTip',
       'mouseleave .tips': 'hideToolTip',
+      'mouseenter .searchbutton': 'fadeSearchUp',
+      'mouseleave .searchbutton': 'fadeSearchDown',
+      'mouseenter .backsearchbutton': 'fadebackUp',
+      'mouseleave .backsearchbutton': 'fadebackDown',
       'searchDesk.done': function(data) {
       /* services.notify(data.results); */
       _.each(data.results, this.list, this);
-    }
+
+            this.switchTo('results',{
+          listArray: this.listArray
+      });
+      },
+      'click .backsearchbutton': 'getBackToSearch'
     },
 
     list: function(item) { 
       // services.notify('#' + item.id + ' ' + item.subject);
-      this.listArray.push({'id': '' + item.id + '','test': + '' + item.subject + ''});
+      this.listArray.push({'PassURL': '' + item.url + '', 'PassId': '' + item.id + '','PassSubject': item.subject });
       this.displaySearch();
-      console.log(1234);
       },
 
 
@@ -31,20 +39,18 @@
           url: '/api/v2/search.json?query=' + data,
           type: 'GET'
         };
+
       }
 
     },
 
     displaySearch: function() {
-      this.switchTo('list', {
-        listArray: this.listArray
-      });
+      this.switchTo('list');
     },
 
     
 
    doTheSearch: function(){
-
 
     var status = null;
 
@@ -64,6 +70,7 @@
     } else {
       priority = null;
     }     
+
 
 
      var dateRange = this.$('#date_action_operator').val() + '>' + this.$('#from_date').val() + ' ' + this.$('#date_action_operator').val() + '<' + this.$('#to_date').val();
@@ -94,6 +101,7 @@
 
      this.data = 'type:ticket' + ' ' + status + ' ' + priority + ' ' + dateRange + ' ' + endDescription;
      this.runSearchNow();
+
    },
 
    showToolTip: function () {
@@ -103,11 +111,41 @@
 
    hideToolTip: function () {
     this.$('.tooltip').fadeOut('fast');
-    console.log("mouse goes out 20 ");   
+    console.log("mouse goes out 22 ");   
+   }, 
+
+   fadeSearchUp: function () {
+    this.$('.searchbutton').fadeTo('fast',1);
+    console.log("mouse goes in search");
+   },
+
+   fadeSearchDown: function () {
+    this.$('.searchbutton').fadeTo('fast',0.5);
+    console.log("mouse goes out search ");   
+   }, 
+
+   fadebackUp: function () {
+    this.$('.backsearchbutton').fadeTo('fast',1);
+    console.log("mouse goes in back");
+   },
+
+   fadebackDown: function () {
+    this.$('.backsearchbutton').fadeTo('fast',0.5);
+    console.log("mouse goes out back ");   
    }, 
 
    runSearchNow: function () {
     this.ajax('searchDesk',this.data);
+   },
+
+   goToResultsNow: function () {
+          this.switchTo('results',{
+          listArray: this.listArray
+      });
+   },
+
+   getBackToSearch: function () {
+    this.switchTo('list');
    }
 
 
